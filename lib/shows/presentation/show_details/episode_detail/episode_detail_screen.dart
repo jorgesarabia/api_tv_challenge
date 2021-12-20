2c10a7aaf59de01f1b8380f2e80476a359c2207f
Jorge Sarabia <jorge@sarabiajor.ge>
2021-12-19 21:25:57 -0300

    Add Episode detail

---
+++ lib/shows/presentation/show_details/episode_detail/episode_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EpisodeDetailScreen extends StatelessWidget {
+  const EpisodeDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
